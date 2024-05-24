0e5b4097d6b3afa3e74112e8decaa6f3efcd9024
rio-98 <1998rio15@gmail.com>
2024-05-24 13:51:50 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const menuItems = document.getElementById('menu-items');
-
-    hamburger.addEventListener('click', function() {
-        menuItems.classList.toggle('expanded');
-    });
-});
